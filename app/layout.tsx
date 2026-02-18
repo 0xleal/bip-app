5e24dbf79618d90ba5fdfee5f9189c54814363b2
Francisco Leal <francisco@talentprotocol.com>
2026-02-18 14:41:03 +0000

    Add talent.app verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,12 @@ import { SessionProvider } from "@/components/session-provider";
 export const metadata: Metadata = {
   title: "Build in Public Tool",
   description: "Share your development work and learnings consistently",
+  other: {
+    "talentapp:project_verification":
+      "767cdb303f09ae57de902f479281454293e7e00cd5dde03ec45ab55a8462b6c3745994f99b3dca33c0cc8739afca6e69a4f816b2299df9f910a75508112a3e9f",
+  },
 };
+
 
 export default function RootLayout({
   children,
